6ee402f3ea434f940142c85b43bbde9343b43769
Jordan Woodson <64621053+jordanwoodson@users.noreply.github.com>
2021-09-06 20:53:45 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,4 @@
+import math
+
+print(math.sqrt(900))
+print(math.floor(9.2))
---
+++ population-dispersion.py
@@ -0,0 +1,4 @@
+import statistics
+
+print(statistics.variance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
+print(statistics.stdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
